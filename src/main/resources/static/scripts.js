9094d23a9c873915549bfc0593212bb74a2a2cce
evegcar <carlos.vega.garcia@ericsson.com>
2019-10-21 04:46:28 +0200

    Add disable btn behaviour when order has empty parameters Part 2

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -2,28 +2,24 @@ var x=2
 function appendTextarea()
 {
 	var title = '<label class="text-black" for="message">Elemento ' + x + ' </label>'
-	var element = '<textarea name="element' + x + '" id="" cols="30" rows="1" class="form-control notEmpty rounded-0" placeholder="Escribe aquí el elemento que quieres"></textarea>';
+	var element = '<textarea name="element' + x + '" id="element' + x + '" cols="30" rows="1" class="form-control notEmpty rounded-0" placeholder="Escribe aquí el elemento que quieres"></textarea>';
 	$('#textareas').append('<br>');
 	$('#textareas').append(title);
 	$('#textareas').append(element);
 	x++;
 	checkDisabled();
-	//$('#append textarea:first').focus();
 }
 $('#sendNewButton').attr('disabled',true);
-$('.notEmpty').focusout(function(){
+$('.formFilled').focusout(function(){
 	checkDisabled();
 });
 
 function checkDisabled() {
 	$('.notEmpty').each(function(i) {
 		if(!$(this).val()){
-			console.log("true");
 			$('#sendNewButton').attr('disabled',true);
 		} else {
 			$('#sendNewButton').attr('disabled',false);
-			console.log("false");
 		}	
 	})			
 }
-//$('#sendNewButton').attr('disabled',true);
