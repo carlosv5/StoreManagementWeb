293a69145918abd4f76a95e05a6ff1c45a6e8501
evegcar <carlos.vega.garcia@ericsson.com>
2019-10-21 05:14:36 +0200

    Add remove btn when there is more than 1 element in order

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,14 +1,27 @@
 var x=2
 function appendTextarea()
 {
-	var title = '<label class="text-black" for="message">Elemento ' + x + ' </label>'
-	var element = '<textarea name="element' + x + '" id="element' + x + '" cols="30" rows="1" class="form-control notEmpty rounded-0" placeholder="Escribe aquí el elemento que quieres"></textarea>';
-	$('#textareas').append('<br>');
+	var title = '<label class="text-black element' + x + '" for="message">Elemento ' + x + ' </label>'
+	var element = '<textarea name="element' + x + '" id="element' + x + '" cols="30" rows="1" class="form-control notEmpty rounded-0 element' + x + '" placeholder="Escribe aquí el elemento que quieres"></textarea>';
+	$('#textareas').append('<br class="element' + x + '">');
 	$('#textareas').append(title);
 	$('#textareas').append(element);
+	var newBtn = '<input value="Borrar elemento" onclick ="removeTextarea()" class="btn btn-black rounded-0 py-1 px-1 element' + x + '">';
+	$('#textareas').append(newBtn);
 	x++;
 	checkDisabled();
 }
+
+function removeTextarea()
+{
+	x--;
+	$(".element" + x).each(function(i) {
+		console.log("removing");
+		$(this).remove();
+	})
+	checkDisabled();
+}
+
 $('#sendNewButton').attr('disabled',true);
 $('.formFilled').focusout(function(){
 	checkDisabled();
@@ -21,5 +34,5 @@ function checkDisabled() {
 		} else {
 			$('#sendNewButton').attr('disabled',false);
 		}	
-	})			
+	})
 }
